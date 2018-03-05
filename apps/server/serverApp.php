e3fafe66f3f1f9d73f427d925343d357240f2084
stingray-11 <115stingray@gmail.com>
2018-03-05 15:11:20 -0500

    add server and tests placeholder apps

---
+++ apps/server/serverApp.php
@@ -0,0 +1,35 @@
+<?php namespace Andromeda\Apps\Server; if (!defined('Andromeda')) { die(); }
+
+require_once(ROOT."/core/AppBase.php"); use Andromeda\Core\AppBase;
+require_once(ROOT."/core/Utilities.php");use Andromeda\Core\Utilities;
+require_once(ROOT."/core/ioformat/Input.php");use Andromeda\Core\IOFormat\Input;
+require_once(ROOT."/core/ioformat/SafeParam.php");use Andromeda\Core\IOFormat\SafeParam;
+
+use Andromeda\Core\UnknownActionException;
+
+class ServerApp extends AppBase
+{
+    public function Run(Input $input)
+    {
+        $action = $input->GetAction();
+        
+        if ($action == 'random')        return $this->Random($input);
+        else if ($action == 'version')  return $this->Version($input);
+        else if ($action == 'getapps')  return $this->GetApps($input);
+        
+        else throw new UnknownActionException();
+    }
+    
+    public function Random(Input $input) : array
+    {
+        $length = $input->TryGetParam("length", SafeParam::TYPE_INT) ?? Utilities::IDLength;
+        
+        $random = Utilities::Random($length);
+        
+        return array('random'=>$random);
+    }
+    
+    public function Version(Input $input) { return VERSION; }
+    public function GetApps(Input $input) { return $this->API->GetServer()->GetApps(); }
+}
+
