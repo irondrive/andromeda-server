468bdbb9f80306b36fcad569d5d1f90edc6c2289
stingray-11 <115stingray@gmail.com>
2018-04-23 20:35:15 -0400

    small bug fix and use self

--- core/database/StandardObject.php
+++ core/database/StandardObject.php
@@ -13,7 +13,7 @@ abstract class StandardObject extends BaseObject
     protected function GetDate(string $name) : ?int                { return $this->GetScalar("dates__$name"); }
     protected function TryGetDate(string $name) : ?int             { return $this->TryGetScalar("dates__$name"); } 
     
-    protected function SetDate(string $name, int $value = null) : StandardObject 
+    protected function SetDate(string $name, int $value = null) : self
     { 
         if ($value === null) $value = time(); 
         return $this->SetScalar("dates__$name", $value); 
@@ -22,9 +22,9 @@ abstract class StandardObject extends BaseObject
     protected function GetFeature(string $name) : ?int             { return $this->GetScalar("features__$name"); }
     protected function TryGetFeature(string $name) : ?int          { return $this->TryGetScalar("features__$name"); }
     protected function ExistsFeature(string $name) : ?int          { return $this->ExistsScalar("features__$name"); }
-    protected function SetFeature(string $name, ?int $value) : StandardObject   { return $this->SetScalar("features__$name", $value); }
-    protected function EnableFeature(string $name) : StandardObject             { return $this->SetScalar("features__$name", 1); }
-    protected function DisableFeature(string $name) : StandardObject            { return $this->SetScalar("features__$name", 0); }
+    protected function SetFeature(string $name, ?int $value) : self   { return $this->SetScalar("features__$name", $value); }
+    protected function EnableFeature(string $name) : self             { return $this->SetScalar("features__$name", 1); }
+    protected function DisableFeature(string $name) : self            { return $this->SetScalar("features__$name", 0); }
     
     protected function GetCounter(string $name) : ?int             { return $this->GetScalar("counters__$name"); }
     protected function TryGetCounter(string $name) : ?int          { return $this->TryGetScalar("counters__$name"); }
@@ -32,9 +32,9 @@ abstract class StandardObject extends BaseObject
     protected function GetCounterLimit(string $name) : ?int        { return $this->GetScalar("counters_limits__$name"); }
     protected function TryGetCounterLimit(string $name) : ?int     { return $this->TryGetScalar("counters_limits__$name"); }
     protected function ExistsCounterLimit(string $name) : ?int     { return $this->ExistsScalar("counters_limits__$name"); }
-    protected function SetCounterLimit(string $name, ?int $value) : StandardObject  { return $this->SetScalar("counters_limits__$name", $value); }
+    protected function SetCounterLimit(string $name, ?int $value) : self  { return $this->SetScalar("counters_limits__$name", $value); }
     
-    protected function DeltaCounter(string $name, int $delta) : StandardObject 
+    protected function DeltaCounter(string $name, int $delta) : self
     { 
         if (($limit = $this->TryGetCounterLimit($name)) !== null) {
             $value = $this->GetCounter($name) + $delta;
@@ -73,7 +73,7 @@ abstract class StandardObject extends BaseObject
 
 abstract class SingletonObject extends StandardObject
 {
-    public static function Load(ObjectDatabase $database) : StandardObject
+    public static function Load(ObjectDatabase $database) : self
     {
         $objects = self::LoadManyMatchingAll($database, null);
         if (count($objects) > 1) throw new DuplicateSingletonException();
