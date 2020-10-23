1018640ff6b823e4453ecf7b8912d6e004cdb919
stringray-11 <115stingray@gmail.com>
2020-10-22 20:07:01 -0400

    add Truncate() and WriteFile() implementations, fix Download FS chunking, fix getting file handles

--- filesystem/NativeCrypt.php
+++ filesystem/NativeCrypt.php
@@ -21,12 +21,15 @@ class NativeCrypt extends Native
     
     private function GetAuthString(File $file, int $index) { return $file->ID().":$index"; }
     
+    public function GetChunkIndex(int $byte) : int { return intdiv($byte, $this->chunksize); }
+    public function GetNumChunks(int $bytes) : int { return $bytes ? intdiv($bytes-1, $this->chunksize)+1 : 0; }
+    
     public function ImportFile(File $file, string $oldpath) : self
     {
         $newpath = parent::GetFilePath($file);
         
         $length = filesize($oldpath);
-        $chunks = intdiv($length, $this->chunksize) + 1;
+        $chunks = $this->GetNumChunks($length);
         
         $this->GetStorage()->CreateFile($newpath);
         $this->Truncate($file, $length, false);
@@ -45,8 +48,102 @@ class NativeCrypt extends Native
         return $this;
     }
     
-    public function ReadChunk(File $file, int $index) : string
+    public function ReadBytes(File $file, int $start, int $length) : string
     {
+        $toend = $start + $length === $file->GetSize();
+        if ($start % $this->chunksize || ($length % $this->chunksize && !$toend))
+            throw new UnalignedAccessException();
+        
+        $length = min($start+$length, $file->GetSize())-$start;
+        
+        $chunk0 = $this->GetChunkIndex($start);
+        $chunks = $this->GetNumChunks($length);        
+        $fchunks = $this->GetNumChunks($file->GetSize());
+        $chunks = min($chunks, $fchunks-$chunk0);
+
+        $data = ""; for ($chunk = $chunk0; $chunk < $chunk0+$chunks; $chunk++)
+            $data .= $this->ReadChunk($file, $chunk);
+        return $data;
+    }
+    
+    public function WriteBytes(File $file, int $start, string $data) : self
+    {
+        $length = strlen($data); $toend = $start + $length === $file->GetSize();
+        if ($start % $this->chunksize || ($length % $this->chunksize && !$toend))
+            throw new UnalignedAccessException();
+        
+        $length = min($start+$length, $file->GetSize())-$start;
+        
+        $chunk0 = $this->GetChunkIndex($start);
+        $chunks = $this->GetNumChunks($length);
+        $fchunks = $this->GetNumChunks($file->GetSize());
+        $chunks = min($chunks, $fchunks-$chunk0);
+        
+        for ($chunk = 0; $chunk < $chunks; $chunk++)
+        {
+            $subdata = substr($data, $chunk*$this->chunksize, $this->chunksize);
+            $this->WriteChunk($file, $chunk+$chunk0, $subdata);
+        }
+        return $this;
+    }
+    
+    public function Truncate(File $file, int $length, bool $init = true) : self
+    {
+        $length = max($length, 0);
+        
+        $noncesize = CryptoAEAD::NonceLength();
+        $extrasize = $noncesize + CryptoAEAD::OutputOverhead();
+        
+        $chunks = $this->GetNumChunks($length);
+        $rlength = $chunks * $extrasize + $length;
+
+        $length0 = $file->GetSize();
+        
+        if ($init && $length != $length0)
+        {            
+            $chunks0 = $this->GetNumChunks($length0);
+            if ($chunks <= $chunks0)
+            {
+                $remain = ($length-1) % $this->chunksize + 1;             
+                if ($remain) $data = $this->ReadChunk($file, $chunks-1);                
+                parent::Truncate($file, $rlength);
+                
+                if ($remain) // rewrite new last chunk
+                {
+                    $data = str_pad(substr($data, 0, $remain), $remain, "\0");
+                    $this->WriteChunk($file, $chunks-1, $data);  
+                }                
+            }
+            else // write new chunks
+            {        
+                if ($chunks0) $data0 = $this->ReadChunk($file, $chunks0-1);
+                parent::Truncate($file, $rlength);
+                
+                if ($chunks0) // rewrite old last chunk
+                {
+                    $data0 = str_pad($data0, $this->chunksize, "\0");
+                    $this->WriteChunk($file, $chunks0-1, $data0);
+                }
+                
+                if ($chunks > $chunks0) // fill new blank chunks
+                {
+                    $datan = str_pad("", $this->chunksize, "\0");
+                    for ($chunk = $chunks0; $chunk < $chunks; $chunk++)
+                    {
+                        if ($chunk == $chunks-1) // trim last chunk
+                            $datan = substr($datan, 0, $length % $this->chunksize);
+                        $this->WriteChunk($file, $chunk, $datan);
+                    }
+                }
+            }
+        }
+        else parent::Truncate($file, $rlength);
+        
+        return $this;
+    }
+    
+    protected function ReadChunk(File $file, int $index) : string
+    {       
         $noncesize = CryptoAEAD::NonceLength();
         $datasize = $this->chunksize + CryptoAEAD::OutputOverhead();
         
@@ -60,14 +157,7 @@ class NativeCrypt extends Native
         return CryptoAEAD::Decrypt($data, $nonce, $this->masterkey, $auth);
     }
 
-    public function ReadBytes(File $file, int $start, int $length) : string
-    {
-        if ($start % $this->chunksize || $length % $this->chunksize)
-            throw new UnalignedAccessException();        
-        return $this->ReadChunk($file, $start / $this->chunksize);
-    }
-    
-    public function WriteChunk(File $file, int $index, string $data) : self
+    protected function WriteChunk(File $file, int $index, string $data) : self
     {
         $noncesize = CryptoAEAD::NonceLength();
         $datasize = $this->chunksize + CryptoAEAD::OutputOverhead();
@@ -83,37 +173,6 @@ class NativeCrypt extends Native
 
         parent::WriteBytes($file, $nonceoffset, $nonce);
         parent::WriteBytes($file, $dataoffset, $data);
-        
-        return $this;
-    }
-    
-    public function WriteBytes(File $file, int $start, string $data) : self
-    {
-        if ($start % $this->chunksize || strlen($data) % $this->chunksize)
-            throw new UnalignedAccessException();
-        $this->WriteChunk($file, $start / $this->chunksize); return $this;
-    }
-    
-    public function Truncate(File $file, int $length, bool $init = true) : self
-    {
-        $noncesize = CryptoAEAD::NonceLength();
-        $extrasize = $noncesize + CryptoAEAD::OutputOverhead();
-
-        $chunks = intdiv($length, $this->chunksize) + 1;
-        $rlength = $chunks * $extrasize + $length;
-
-        parent::Truncate($file, $rlength);
-
-        if ($init)
-        {
-            $data = str_pad("", $this->chunksize, "\0");
-            for ($chunk = 0; $chunk < $chunks-1; $chunk)
-               $this->WriteChunk($file, $chunk, $data);
-            
-            $remain = $length % $this->chunksize;
-            $data = substr($data, 0, $remain);
-            $this->WriteChunk($file, $chunks-1, $data);
-        }
         
         return $this;
     }
