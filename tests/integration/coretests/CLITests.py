227a423b3796b00b3caaa7c0c9ccc163ebc9c51f
stringray-11 <115stingray@gmail.com>
2021-07-31 23:52:38 +0000

    Add dot printing to tests

--- tests/integration/coretests/CLITests.py
+++ tests/integration/coretests/CLITests.py
@@ -5,7 +5,7 @@ class CLITests(BaseTest):
 
     # TODO test batching (testApp?), --dryrun
 
-    def basicInvalid(self, format=None, debug=None, metrics=None, dbconf=None):
+    def basicInvalid(self, format=None, debug=0, metrics=0, dbconf=None):
         flags = []
         if format is not None: flags.append('--'+format)
         if debug is not None: flags += ['--debug',str(debug)]
@@ -14,33 +14,33 @@ class CLITests(BaseTest):
         return self.interface.cliRun('none','none',{},{},flags)
     
     def testDebugFlag(self):
-        rval = self.basicInvalid("json", 0)
+        rval = self.basicInvalid(format="json")
         assertEquals(rval['code'], 400)
         assertNotIn('debug', rval)
 
-        rval = self.basicInvalid("json", 2)
+        rval = self.basicInvalid(format="json", debug=2)
         assertEquals(rval['code'], 400)
         assertIn('debug', rval)
 
     def testFormatFlag(self):
-        rval = self.basicInvalid(None, 0)
+        rval = self.basicInvalid(debug=0)
         assertInstance(rval, str)
         assertEquals(rval.strip(),"UNKNOWN_APP")
 
-        rval = self.basicInvalid("json", 0)
+        rval = self.basicInvalid(format="json")
         assertInstance(rval, object)
         assert(not rval['ok'])
 
-        rval = self.basicInvalid("printr", 0)
+        rval = self.basicInvalid(format="printr")
         assertInstance(rval, str)
         assert(rval.startswith("Array")), rval
 
     def testMetricsFlag(self):
-        assertNotIn('metrics', self.basicInvalid("json", 0, 0))
-        assertIn('metrics', self.basicInvalid("json", 0, 1))
+        assertNotIn('metrics', self.basicInvalid(format="json"))
+        assertIn('metrics', self.basicInvalid(format="json", metrics=1))
 
     def testDbconfFlag(self):
-        rval = self.basicInvalid("json", 1, 0, "/nonexistent")
+        rval = self.basicInvalid(format="json", debug=1, dbconf="/nonexistent")
         assertError(rval, 500, "SERVER_ERROR")
         assertEquals(rval['debug']['message'], 'DATABASE_CONFIG_MISSING')
 
