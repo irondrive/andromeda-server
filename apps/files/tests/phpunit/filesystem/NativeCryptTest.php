5b8b35275f1f2db0737913efb4807f2ef470dbfd
stringray-11 <115stingray@gmail.com>
2021-07-31 13:34:19 +0000

    more thorough NativeCrypt unit testing

--- apps/files/tests/phpunit/filesystem/NativeCryptTest.php
+++ apps/files/tests/phpunit/filesystem/NativeCryptTest.php
@@ -19,15 +19,6 @@ class NativeCryptTest extends \PHPUnit\Framework\TestCase
     private Storage $storage;
     private NativeCrypt $fsimpl;
     private FSManager $filesystem;
-        
-    protected function getTmpFile() : string
-    {
-        $file = tempnam(sys_get_temp_dir(), 'a2test');
-        
-        $this->assertNotFalse($file);
-        
-        $this->files[] = $file; return $file;
-    }
     
     public function setUp() : void
     {        
@@ -35,54 +26,40 @@ class NativeCryptTest extends \PHPUnit\Framework\TestCase
         
         $storage->method('CreateFile')->will($this->returnCallback(function(string $path)use($storage)
         {
-            $file = $this->getTmpFile();
-            
-            $this->paths[$path] = fopen($file,'rb+');
+            $this->paths[$path] = fopen("php://memory",'rb+');
             
             return $storage;
         }));
         
         $storage->method('ReadBytes')->will($this->returnCallback(function(string $path, int $start, int $length)
         {
-            $this->assertArrayHasKey($path, $this->paths);
-            
-            $handle = $this->paths[$path];
-            
-            fseek($handle, $start); 
+            $handle = $this->paths[$path];            
+            fseek($handle, $start);
             
             return fread($handle, $length);
         }));
         
         $storage->method('WriteBytes')->will($this->returnCallback(function(string $path, int $start, string $data)use($storage)
         {
-            $this->assertArrayHasKey($path, $this->paths);
-            
             $handle = $this->paths[$path];
-            
-            fseek($handle, $start);
-            
-            fwrite($handle, $data);
+            fseek($handle, $start);            
+            fwrite($handle, $data);  
             
             return $storage;
         }));
         
         $storage->method('Truncate')->will($this->returnCallback(function(string $path, int $length)use($storage)
         {
-            $this->assertArrayHasKey($path, $this->paths);
-            
-            $handle = $this->paths[$path];
-            
-            ftruncate($handle, $length);
+            $handle = $this->paths[$path];            
+            ftruncate($handle, $length);  
             
             return $storage;
         }));
         
-        $this->storage = $storage;
+        $filesystem = $this->createMock(FSManager::class);
+        $filesystem->method('GetStorage')->willReturn($storage);
         
-        $this->filesystem = $this->createMock(FSManager::class);
-        $this->filesystem->method('GetStorage')->willReturn($this->storage);
-        
-        $this->fsimpl = new NativeCrypt($this->filesystem, CryptoSecret::GenerateKey(), self::CHUNK_SIZE);
+        $this->fsimpl = new NativeCrypt($filesystem, CryptoSecret::GenerateKey(), self::CHUNK_SIZE);
     }
 
     public function tearDown() : void
@@ -90,6 +67,13 @@ class NativeCryptTest extends \PHPUnit\Framework\TestCase
         foreach ($this->paths as $handle) fclose($handle);
         
         foreach ($this->files as $file) unlink($file);
+    }    
+    
+    protected function getTmpFile() : string
+    {
+        $file = tempnam(sys_get_temp_dir(), 'a2test');
+        
+        $this->files[] = $file; return $file;
     }
     
     protected function createFile(string $data) : File
@@ -101,88 +85,112 @@ class NativeCryptTest extends \PHPUnit\Framework\TestCase
         
         $path = self::getTmpFile(); file_put_contents($path, $data);
         
-        $file->method('SetSize')->will($this->returnCallback(function($size)use($file){
+        $file->method('SetSize')->will($this->returnCallback(function(int $size,bool $notify)use($file)
+        {
+            if (!$notify) $this->fsimpl->Truncate($file, $size);
+            
             $this->sizes[$file->ID()] = $size; return $file;
         }));
         
-        $file->method('GetSize')->will($this->returnCallback(function()use($file){
+        $file->method('GetSize')->will($this->returnCallback(function()use($file)
+        {
             return $this->sizes[$file->ID()];
         }));
         
-        $this->fsimpl->ImportFile($file->SetSize(strlen($data)), $path);
+        $this->fsimpl->ImportFile($file->SetSize(strlen($data),true), $path);
         
         return $file;
     }
     
-    // truncate makes thorough use of import/read/write!
-    protected function tryTruncate(int $size0, int $size) : void
+    protected function tryWriting(int $size0, int $offset, int $wlen) : void
     {
         // first import a mock file and check its contents
         
-        $data = Utilities::Random($size0);
-        
+        $data = Utilities::Random($size0);        
         $file = $this->createFile($data);
         
-        $ret = $this->fsimpl->ReadBytes($file, 0, $size0);
-        
-        $this->assertSame($data, $ret); 
-        
-        // next truncate the file and check its new contents
-        
-        if ($size < $size0) $data = substr($data, 0, $size);
-        else if ($size > $size0) $data = str_pad($data, $size, "\0");
-        
-        $this->assertSame($size, strlen($data));
-        
-        $this->fsimpl->Truncate($file, $size); $file->SetSize($size);
-        
-        $ret = $this->fsimpl->ReadBytes($file, 0, $size);
-        
+        $ret = $this->fsimpl->ReadBytes($file, 0, $size0);        
         $this->assertSame($data, $ret);
+        
+        // next write to the file and check its new contents
+        // if offset0 > size0, WriteBytes runs SetSize (Truncate)
+        
+        $size = max($size0, $offset + $wlen);        
+        if ($size > $size0) $data = str_pad($data, $size, "\0");
+        
+        $wdata = Utilities::Random($wlen);
+        
+        $data = substr($data, 0, $offset).$wdata.substr($data, $offset+$wlen);        
+        $this->fsimpl->WriteBytes($file, $offset, $wdata); $file->SetSize($size,true);
+
+        $ret = $this->fsimpl->ReadBytes($file, 0, $size);
+        $this->assertSame($data, $ret);
+        
+        if ($size > $size0) // test shrinking back to size0
+        {
+            $data = substr($data, 0, $size0);
+            $this->fsimpl->Truncate($file, $size0); $file->SetSize($size0, true);
+            
+            $ret = $this->fsimpl->ReadBytes($file, 0, $size0);
+            $this->assertSame($data, $ret);
+        }
     }
     
-    protected function tryTruncatePair(int $size0, int $size) : void
+    protected function tryWritingPair(int $size0, int $size) : void
     {
-        $this->tryTruncate($size0, $size);
-        $this->tryTruncate($size, $size0);
+        $this->tryWriting($size, intval($size-$size0)/2, $size0);   // import, write inside file
+        $this->tryWriting($size, $size0, $size);                    // import, write from inside EOF, shrink
+        $this->tryWriting($size0, $size, $size0);                   // import, extend, write at EOF, shrink
     }   
     
     const CHUNK_SIZE = 10; // bytes
     
-    public function testTruncates() : void
+    public function testTruncateZeroes() : void
     {
-        $this->tryTruncate(0, 0);
-        
-        $this->tryTruncatePair(0, 5);
-        $this->tryTruncatePair(0, 55);
-        
-        // extend/shrink within a chunk
-        $this->tryTruncatePair(3, 7);
-        $this->tryTruncatePair(43, 46);
-        $this->tryTruncatePair(11, 18);
-        $this->tryTruncatePair(14, 20);
-        $this->tryTruncatePair(11, 20);
-        
-        // extend/shrink one chunk
-        $this->tryTruncatePair(0, 10);
-        $this->tryTruncatePair(3, 10);
-        $this->tryTruncatePair(9, 10);
-        $this->tryTruncatePair(10, 25);
-        $this->tryTruncatePair(14, 25);
-        $this->tryTruncatePair(19, 25);
-        $this->tryTruncatePair(20, 39);
-        $this->tryTruncatePair(27, 39);
-        $this->tryTruncatePair(29, 39);
-        
-        // extend/shrink multiple chunks
-        $this->tryTruncatePair(0, 50);
-        $this->tryTruncatePair(3, 50);
-        $this->tryTruncatePair(9, 50);
-        $this->tryTruncatePair(10, 77);
-        $this->tryTruncatePair(14, 77);
-        $this->tryTruncatePair(19, 77);
-        $this->tryTruncatePair(20, 99);
-        $this->tryTruncatePair(27, 99);
-        $this->tryTruncatePair(29, 99);
+        $this->tryWriting(0, 0, 0);
+        $this->tryWritingPair(0, 1);
+        $this->tryWritingPair(0, 5);
+        $this->tryWritingPair(0, 9);
+        $this->tryWritingPair(0, 10);
+        $this->tryWritingPair(0, 11);
+        $this->tryWritingPair(0, 55);
+        $this->tryWritingPair(0, 59);
+        $this->tryWritingPair(0, 60);
+        $this->tryWritingPair(0, 61);
+    }
+    
+    public function testTruncateWithinChunk() : void
+    {
+        $this->tryWritingPair(3, 7);
+        $this->tryWritingPair(43, 46);
+        $this->tryWritingPair(11, 18);
+        $this->tryWritingPair(14, 20);
+        $this->tryWritingPair(11, 20);
+    }
+    
+    public function testTruncateOneChunk() : void
+    {
+        $this->tryWritingPair(3, 10);
+        $this->tryWritingPair(9, 10);
+        $this->tryWritingPair(10, 25);
+        $this->tryWritingPair(14, 25);
+        $this->tryWritingPair(19, 25);
+        $this->tryWritingPair(40, 59);
+        $this->tryWritingPair(41, 59);
+        $this->tryWritingPair(47, 59);
+        $this->tryWritingPair(49, 59);
+    }
+    
+    public function testTruncateManyChunks() : void
+    {
+        $this->tryWritingPair(3, 50);
+        $this->tryWritingPair(9, 50);
+        $this->tryWritingPair(10, 77);
+        $this->tryWritingPair(14, 77);
+        $this->tryWritingPair(19, 77);
+        $this->tryWritingPair(40, 99);
+        $this->tryWritingPair(41, 99);
+        $this->tryWritingPair(47, 99);
+        $this->tryWritingPair(49, 99);
     }
 }
