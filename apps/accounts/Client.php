73bb6cde583adb96a00e055f8c4c14b9e176ce1e
stingray-11 <115stingray@gmail.com>
2021-10-05 20:49:36 +0000

    use dates__active for pruning

--- apps/accounts/Client.php
+++ apps/accounts/Client.php
@@ -74,7 +74,7 @@ class Client extends AuthObject
         $mintime = Main::GetInstance()->GetTime() - $maxage;
         
         $q = new QueryBuilder(); $q->Where($q->And(
-            $q->Equals('account',$account->ID()),$q->LessThan('dates__created', $mintime)));
+            $q->Equals('account',$account->ID()),$q->LessThan('dates__active', $mintime)));
         
         static::DeleteByQuery($database, $q);
     }
