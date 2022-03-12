78364f9fefbe6c8f84421f6da5431c9f541089f3
stringray-11 <115stingray@gmail.com>
2022-03-12 10:53:51 -0500

    rewrite the database (work in progress) ... stronger typing, better concurrency

--- Andromeda/Core/Database/DBStats.php
+++ Andromeda/Core/Database/DBStats.php
@@ -2,21 +2,30 @@
 
 /**
  * This class keeps track of performance metrics for the database.
- * 
  * Such metrics include read/write query count, and read/write time.
  */
 class DBStats
 {
+    private float $query_start;
+    private float $start_timens;
+    private float $total_timens;
+    
     private int $reads = 0; 
     private int $writes = 0; 
-    private float $start_time;
-    private float $query_start;
     private float $read_time = 0; 
     private float $write_time = 0; 
+    
+    /**@var array<array> */
     private array $queries = array();
     
+    public const QUERY_READ = 1;
+    public const QUERY_WRITE = 2;
+    
     /** Constructs a new stats context and logs the current time */
-    public function __construct(){ $this->start_time = hrtime(true); }
+    public function __construct(){ $this->start_timens = hrtime(true); }
+    
+    /** Stops the overall timers */
+    public function stopTiming(){ $this->total_timens = hrtime(true) - $this->start_timens; }
 
     /** Begins tracking a query by logging the current time */
     public function startQuery() : void { $this->query_start = hrtime(true); }
@@ -31,39 +40,42 @@ class DBStats
     { 
         $el = (hrtime(true)-$this->query_start)/1e9;
         
-        $isRead = $type & Database::QUERY_READ;
-        $isWrite = $type & Database::QUERY_WRITE;
+        $isRead = $type & self::QUERY_READ;
+        $isWrite = $type & self::QUERY_WRITE;
         
         if ($isRead && $isWrite) $el /= 2;
         
         if ($isRead) { $this->read_time += $el; if ($count) $this->reads++; }
         if ($isWrite) { $this->write_time += $el; if ($count) $this->writes++; }
      
-        $this->queries[] = array('query'=>$sql, 'time'=>$el);
+        $this->queries[] = array('query'=>$sql,'time'=>$el);
     }
     
     /** 
      * Returns the array of queries issued to the database 
-     * @return string[]
+     * @return array<array>
      */
-    public function getQueries() : array { return $this->queries; }
+    public function getQueries() : array   { return $this->queries; }
+
+    /** Return the number of database reads */
+    public function GetReads() : int       { return $this->reads; }
     
-    /** 
-     * Returns an array of statistics collected 
-     * @return array `{db_reads:int,db_read_time:double,db_writes:int,db_write_time:double,code_time:double,total_time:double}`
-     */
-    public function getStats() : array
+    /** Return the time spent reading from the database */
+    public function GetReadTime() : float  { return $this->read_time; }
+    
+    /** Return the number of database writes */
+    public function GetWrites() : int      { return $this->writes; }
+    
+    /** Return the time spend writing to the database */
+    public function GetWriteTime() : float { return $this->write_time; }
+    
+    /** Return the total elapsed time */
+    public function GetTotalTime() : float { return $this->total_timens/1e9; }
+    
+    /** Return the total non-query (code) time */
+    public function GetCodeTime() : float
     {
-        $totaltime = (hrtime(true)-$this->start_time)/1e9;
-        $codetime = $totaltime - $this->read_time - $this->write_time;
-        return array(
-            'db_reads' => $this->reads,
-            'db_read_time' => $this->read_time,
-            'db_writes' => $this->writes,
-            'db_write_time' => $this->write_time,
-            'code_time' => $codetime,
-            'total_time' => $totaltime
-        );
+        return $this->GetTotalTime() - $this->read_time - $this->write_time;
     }
     
     /** Adds another DBStats' stats to this one */
---
+++ Andromeda/Core/_tests/phpunit/Database/testObjects.php
@@ -0,0 +1,269 @@
+<?php namespace Andromeda\Core\Database; 
+
+require_once("init.php");
+
+require_once(ROOT."/Core/Database/ObjectDatabase.php");
+require_once(ROOT."/Core/Database/FieldTypes.php");
+require_once(ROOT."/Core/Database/BaseObject.php");
+require_once(ROOT."/Core/Database/TableTypes.php");
+
+class OnDelete { public function Delete() : void { } }
+
+final class EasyObject extends BaseObject
+{
+    use TableNoChildren;
+    
+    public static function Create(ObjectDatabase $database) : self
+    {
+        return parent::BaseCreate($database);
+    }
+    
+    private FieldTypes\NullIntType $uniqueKey;
+    private FieldTypes\NullIntType $generalKey;
+    
+    protected function CreateFields() : void
+    {
+        $fields = array();
+        
+        $this->uniqueKey = $fields[] = new FieldTypes\NullIntType('uniqueKey');
+        $this->generalKey = $fields[] = new FieldTypes\NullIntType('generalKey');
+
+        $this->RegisterFields($fields, self::class);
+        
+        parent::CreateFields();
+    }
+    
+    public function Delete() : void { parent::Delete(); }
+    
+    public function GetUniqueKey() : ?int { return $this->uniqueKey->GetValue(); }
+    public function SetUniqueKey(?int $val) : self { $this->uniqueKey->SetValue($val); return $this; }
+    
+    public function GetGeneralKey() : ?int { return $this->generalKey->GetValue(); }
+    public function SetGeneralKey(?int $val) : self { $this->generalKey->SetValue($val); return $this; }
+}
+
+abstract class PolyObject_ extends BaseObject { }
+
+abstract class PolyObject0 extends PolyObject_
+{
+    // empty, no table!
+    
+    /** @return ?array<class-string<self>> */
+    public static function GetChildMap() : ?array
+    {
+        return array(PolyObject1::class); // so we can load via it
+    }
+    
+    private ?OnDelete $ondel = null;
+    
+    public function SetOnDelete(OnDelete $del) : void { $this->ondel = $del; }
+    
+    public function notifyDeleted() : void
+    {
+        if ($this->ondel) $this->ondel->Delete();
+        
+        parent::notifyDeleted();
+    }
+}
+
+abstract class PolyObject1 extends PolyObject0
+{
+    use TableLinkedChildren;
+    
+    /** @return ?array<class-string<self>> */
+    public static function GetChildMap() : ?array
+    {
+        return array(27=>PolyObject2::class); // skip over PolyObject15
+    }
+    
+    private FieldTypes\IntType $testprop1;
+
+    protected function CreateFields() : void
+    {
+        $fields = array();
+        
+        $this->testprop1 = $fields[] = new FieldTypes\IntType('testprop1');
+        
+        $this->RegisterFields($fields, self::class);
+        
+        parent::CreateFields();
+    }
+    
+    public function GetTestProp1() : int
+    { 
+        return $this->testprop1->GetValue(); 
+    }
+    
+    public function SetTestProp1(int $val) : self
+    {
+        $this->testprop1->SetValue($val); return $this;
+    }
+}
+
+abstract class PolyObject15 extends PolyObject1
+{
+    // not part of the "loadable" chain - transparent to DB
+    
+    private FieldTypes\IntType $testprop15;
+    
+    protected function CreateFields() : void
+    {
+        $fields = array();
+        
+        $this->testprop15 = $fields[] = new FieldTypes\IntType('testprop15');
+        
+        $this->RegisterFields($fields); // part of PolyObject2's table
+        
+        parent::CreateFields();
+    }
+    
+    public function GetTestProp15() : int
+    {
+        return $this->testprop15->GetValue();
+    }
+    
+    public function SetTestProp15(int $val) : self
+    {
+        $this->testprop15->SetValue($val); return $this;
+    }
+}
+
+abstract class PolyObject2 extends PolyObject15
+{
+    use TableLinkedChildren;
+
+    /** @return ?array<class-string<self>> */
+    public static function GetChildMap() : ?array
+    {
+        return array(27=>PolyObject3::class);
+    }
+    
+    protected function CreateFields() : void
+    {
+        $fields = array();
+        
+        $this->RegisterFields($fields, self::class);
+        
+        parent::CreateFields();
+    }
+}
+
+abstract class PolyObject3 extends PolyObject2
+{
+    // does not have its own table! but is part of the loadable chain
+    
+    /** @return ?array<class-string<self>> */
+    public static function GetChildMap() : ?array
+    {
+        return array(PolyObject4::class);
+    }
+}
+
+class PolyObject4 extends PolyObject3
+{
+    use TableTypedChildren;
+
+    /** @return ?array<class-string<self>> */
+    public static function GetChildMap() : ?array
+    {
+        // includes self::class - this class is not abstract!
+        return array(5=>self::class, 13=>PolyObject5a::class, 18=>PolyObject5b::class);
+    }
+    
+    private FieldTypes\IntType $testprop4;
+    private FieldTypes\NullIntType $testprop4n; // null
+    private FieldTypes\Counter $testprop4c; // counter
+    
+    protected function CreateFields() : void
+    {
+        $fields = $this->GetTypeFields();
+        
+        $this->testprop4  = $fields[] = new FieldTypes\IntType('testprop4');
+        $this->testprop4n = $fields[] = new FieldTypes\NullIntType('testprop4n');
+        $this->testprop4c = $fields[] = new FieldTypes\Counter('testprop4c');
+        
+        $this->RegisterFields($fields, self::class);
+        
+        parent::CreateFields();
+    }
+    
+    public function GetTestProp4() : int
+    {
+        return $this->testprop4->GetValue();
+    }
+    
+    public function SetTestProp4(int $val) : self
+    {
+        $this->testprop4->SetValue($val); return $this;
+    }
+    
+    public function SetTestProp4n(?int $val) : self
+    {
+        $this->testprop4n->SetValue($val); return $this;
+    }
+    
+    public function DeltaTestProp4c(int $val) : self
+    {
+        $this->testprop4c->DeltaValue($val); return $this;
+    }
+}
+
+class PolyObject5a extends PolyObject4
+{   
+    use TableTypedChildren;
+    
+    /** @return ?array<class-string<self>> */
+    public static function GetChildMap() : ?array
+    {
+        // includes self::class - this class is not abstract!
+        return array(100=>self::class, 101=>PolyObject5aa::class, 102=>PolyObject5ab::class);
+    }
+
+    public static function Create(ObjectDatabase $database) : self
+    {
+        return self::BaseCreate($database);
+    }
+    
+    private FieldTypes\IntType $testprop5;
+    
+    protected function CreateFields() : void
+    {
+        $fields = $this->GetTypeFields();
+        
+        $this->testprop5 = $fields[] = new FieldTypes\IntType('testprop5');
+        
+        $this->RegisterFields($fields, self::class);
+        
+        parent::CreateFields();
+    }
+    
+    public function GetTestProp5() : int
+    {
+        return $this->testprop5->GetValue();
+    }
+    
+    public function SetTestProp5(int $val) : self
+    {
+        $this->testprop5->SetValue($val); return $this;
+    }
+    
+    public function Delete() : void
+    {
+        parent::Delete();
+    }
+}
+
+final class PolyObject5aa extends PolyObject5a
+{
+    use NoChildren;
+}
+
+final class PolyObject5ab extends PolyObject5a
+{
+    use NoChildren;
+}
+
+final class PolyObject5b extends PolyObject4
+{
+    use NoChildren;
+}
